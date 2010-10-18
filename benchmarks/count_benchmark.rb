8b9b780dd89da71dd3474ce5734bfa1c392bcf46
Peter T. Brown <peter@flippyhead.com>
2010-10-18 10:33:11 -0700

    adding count benchmark

--- benchmarks/count_benchmark.rb
+++ benchmarks/count_benchmark.rb
@@ -2,11 +2,13 @@ require 'benchmarks/benchmark_helper'
 
 @owner = 'owner'
 RedisSpecHelper.reset
-DataBuilder.build(10000)
+DataBuilder.build(1000)
 
-Benchmark.bmbm do |b|
+@results = {}
+
+Benchmark.bm do |b|
   b.report('counting activities') do
-    SirTracksAlot::Count.count(:owner => @owner)
+    SirTracksAlot::Count.count({:owner => @owner}, {:limit => 999999}, :daily)
   end
 
   b.report('building rows for reports') do
@@ -21,5 +23,11 @@ Benchmark.bmbm do |b|
     SirTracksAlot::Count.rows(:owner => @owner, :target => /.+/)
   end
 end
+
+counts_count = SirTracksAlot::Count.all.size
+summaries_count = SirTracksAlot::Summary.all.size
+activities_count = SirTracksAlot::Activity.all.size
+
+puts "*** Generated #{activities_count}, #{counts_count} counts (#{(counts_count+0.1)/(activities_count+0.1)}), #{summaries_count} summaries"
 
 RedisSpecHelper.reset
